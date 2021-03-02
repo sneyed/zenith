56e55d4bb4b1a5a5baffc037ca8f2242904d55f8
Loki <sneyed@gmail.com>
2021-03-02 11:09:54 +1000

    envCheck

--- src/lib/nopublish.js
+++ src/lib/nopublish.js
@@ -1,2 +1,4 @@
-console.log("Please use `npm run deploy` to publish the package");
-process.exit(1);
+if (!process.env["ZENITH_DEPLOY"]) {
+  console.log("Please use `npm run deploy` to publish the package");
+  process.exit(1);
+}
